5fb782e47832779cdd46a8f276027b143691a6c0
curtdragon <tealturboser@gmail.com>
2019-09-19 21:21:59 -0500

    Built post, put, get and delete endpoints

---
+++ foodiefun/food-model.js
@@ -0,0 +1,56 @@
+const db = require("../database/db-config");
+
+module.exports = {
+    add,
+    update,
+    remove,
+    find,
+    // findById,
+    // findByRestaurant,
+    // findByPrice,
+    // findByType,
+    // findByDate,
+    // findByRating    
+}
+
+function add(foodentry) {
+    return db("foodentries").insert(foodentry)
+}
+
+function update(changes, id) {
+    return db("foodentries").where({id}).update(changes)
+}
+
+function remove(id) {
+    return db("foodentries").where({id}).del();
+}
+
+//List of all food entries
+function find() {
+    return db("foodentries")
+}
+
+// //Single food item by ID
+// function findById(id) {
+//     return db("foodentries").where({id}).first();
+// }
+
+// function findByRestaurant(restaurant) {
+
+// }
+
+// function findByPrice(price) {
+
+// }
+
+// function findByType(type) {
+
+// }
+
+// function findByDate(date) {
+
+// }
+
+// function findByRating(rating) {
+
+// }
---
+++ foodiefun/food-router.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const Food = require("./food-model");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    Food.find()
+    .then(food => {
+        res.json(food);
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "Failed to get food posts"
+        });
+    });
+});
+
+// router.get("/:id", (req, res) => {
+//     const {id} = req.params;
+//     Food.findById(id)
+//     .then(food => {
+//         if (food) {
+//             res.json(food);
+//         } else {
+//             res.status(404).json({
+//                 message: "Could not find food entry by given ID"
+//             });
+//         }
+//     })
+//     .catch(err => {
+//         res.status(500).json({
+//             message: "Failed to get food entry"
+//         });
+//     });
+// });
+
+router.post("/", (req, res) => {
+    const foodData = req.body;
+
+    Food.add(foodData)
+    .then(newFood => {
+        res.status(201).json(newFood);
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "Failed to create new food entry"
+        });
+    });
+});
+
+router.put("/:id", (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    Food.update(changes, id)
+    .then(food => {
+        if (food) {
+            res.json({
+                updated: food
+            })
+        } else {
+            res.status(404).json({
+                message: "Could not find food item with given ID"
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "Failed to update food item"
+        })
+    })
+})
+
+router.delete("/:id", (req, res) => {
+    const {id} = req.params;
+
+    Food.remove(id)
+    .then (count => {
+        if (count) {
+            res.json({
+                removed: count
+            });
+        } else {
+            res.status(404).json({
+                message: "Could not find food item with given ID"
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "Failed to delete food item"
+        })
+    })
+})
+
+module.exports = router;
