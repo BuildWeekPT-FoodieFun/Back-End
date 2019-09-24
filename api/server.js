74a19cc382c2783449de371776771ec51f536bdd
curtdragon <tealturboser@gmail.com>
2019-09-23 21:30:12 -0500

    Updated server.js, created restricted middleware and authrouter. Updated food model and router to find by ID

--- api/server.js
+++ api/server.js
@@ -3,7 +3,8 @@ const helmet = require("helmet");
 const cors = require("cors");
 require("dotenv").config();
 
-// const authRouter = require("../auth/auth-router");
+const authenticate= require("../auth/restricted-middleware");
+const authRouter = require("../auth/auth-router");
 const foodRouter = require("../foodiefun/food-router");
 
 const server = express();
@@ -12,13 +13,11 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// server.use("/api/auth", authRouter);
-server.use("/api/app", foodRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/app", authenticate, foodRouter);
 
 server.get("/", (req, res) => {
-    res.status(200).json({ 
-        message: "Server operational"
-    })
+    res.send("Server operational")
 })
 
 module.exports = server;
